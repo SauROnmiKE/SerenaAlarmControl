2aa93a43b88684880b6403a8e31f456ff51e6ec4
SauROnmiKE <sauronmike@gmail.com>
2019-09-05 22:02:15 +0300

    3.0.2 Version
    5 / 9 / 2019
    
    - Fixed bug where passwords from SD didn't load when board powered on from crash.
    - Fixed bug where board would resume from the fully disarmed main menu if reset right after the 5-digit pass input.

---
+++ Serena_302/Serena_Test/Debug/Serena_Test.cpp.d
@@ -0,0 +1,15 @@
+C:\Users\airgeorge-home\AppData\Local\Temp\VMBuilds\Serena_Test\mega_atmega2560\Debug\Serena_Test.cpp.o: \
+ C:\Users\airgeorge-home\AppData\Local\Temp\VMBuilds\Serena_Test\mega_atmega2560\Debug\Serena_Test.cpp \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/arduino.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/binary.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/WCharacter.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/WString.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/HardwareSerial.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Stream.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Print.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Printable.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/USBAPI.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Arduino.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\variants\mega/pins_arduino.h \
+ C:\Users\airgeorge-home\Documents\Arduino\libraries\Graphics/Graphics.h \
+ C:\Users\airgeorge-home\Documents\Arduino\libraries\Security/Security.h
---
+++ Serena_302/Serena_Test/slave/slave.ino
@@ -0,0 +1,46 @@
+#include <SoftwareSerial.h>
+SoftwareSerial BTserial(10, 11); // TX - RX
+
+int relayPin = 8;
+const byte btstate = 13;
+
+bool state = false;
+
+void setup() {
+  // initialize digital pin 8 as an output.
+
+  Serial.begin(9600);
+  BTserial.begin(38400);
+
+  pinMode(relayPin, OUTPUT);
+  pinMode(btstate, INPUT);
+
+}
+
+void loop()
+{
+  // Timer for checking lost connection (solves instant arming when the slave is powered and not connected to master yet - 3 or 5 sec)
+  if (digitalRead(btstate) == LOW)
+  {
+    digitalWrite(relayPin, HIGH);
+//    state = false;
+  }
+
+  if (BTserial.available() > 0)
+  { // Checks whether data is comming from the serial port
+    state = BTserial.read(); // Reads the data from the serial port
+
+    // Controlling the LED
+    if (state == true)
+    {
+      digitalWrite(relayPin, HIGH); // ALARM ON
+//      state = false;
+    }
+    else if (state == false)
+    {
+      digitalWrite(relayPin, LOW); // ALARM OFF
+//      state = false;
+    }
+  }
+
+}
